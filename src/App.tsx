b630952af20b995e105bd1ae2d98eca8b2e23eb6
mina bessada <mina.adibe@gmail.com>
2022-08-05 15:02:55 +0200

    feat(project): add react router package - add home and user profile rotes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+
+import Layout from "./components/layout/Layout";
+
+import Page404 from "./pages/404";
+import HomePage from "./pages/HomePage";
+import UserProfile from "./pages/UserProfile";
 
 function App() {
-  return <div>starter project</div>;
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/user/:username" element={<UserProfile />} />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </Layout>
+  );
 }
 
 export default App;
