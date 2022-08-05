e9363828857af09503581c4a4e26fd9333f74114
mina bessada <mina.adibe@gmail.com>
2022-08-05 15:03:45 +0200

    feat(project): add commitlint package

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,30 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+module.exports = {
+  /*
+   * Resolve and load @commitlint/config-conventional from node_modules.
+   * Referenced packages must be installed
+   */
+  extends: ["@commitlint/config-conventional"],
+  /*
+   * Any rules defined here will override rules from @commitlint/config-conventional
+   */
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "improvement",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+  },
+};
