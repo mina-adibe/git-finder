fbd0a3e377e6b3fcbced139373cacdcb559ac9b8
mina bessada <mina.adibe@gmail.com>
2022-08-05 10:40:29 +0200

    feat(hooks folder): add debounce hook and a hook for redux methods (useSelector , useDispatch)

---
+++ src/hooks/useDebounce.tsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+
+const useDebounce = (data: string, delay: number): string => {
+  const [debouncedData, setDebouncedData] = useState(data);
+
+  useEffect(() => {
+    const debouncedData = setTimeout(() => {
+      setDebouncedData(data);
+    }, delay);
+
+    return () => {
+      clearTimeout(debouncedData);
+    };
+  }, [data, delay]);
+
+  return debouncedData;
+};
+
+export default useDebounce;
+
+console.log("debounce executed");
