c9401422ef08687169e0f4664fb5035699cc7452
mina bessada <mina.adibe@gmail.com>
2022-08-05 20:38:55 +0200

    feat(header): add header component

--- src/components/haeder/Header.tsx
+++ src/components/haeder/Header.tsx
@@ -1,21 +1,50 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, Box } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Box,
+  FormGroup,
+  FormControlLabel,
+  Switch,
+} from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
 
+import { toggleTheme } from "../../store/themeSlice";
+import { useAppDispatch } from "../../hooks/useAppDispatch";
+import { useAppSelector } from "../../hooks/useAppSelector";
+
 const Header = () => {
+  const dispatch = useAppDispatch();
+  const theme = useAppSelector((state) => state.theme);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(toggleTheme());
+  };
+
   return (
-    <>
+    <React.Fragment>
       <AppBar>
-        <Toolbar>
-          <Box>
-            <GitHubIcon />
+        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Box sx={{ display: "flex", alignItems: "center", gap: "10px" }}>
+            <Box>
+              <GitHubIcon />
+            </Box>
+            <Typography variant="h6" component="h1">
+              Git Finder
+            </Typography>
           </Box>
-          <Typography variant="h6" component="h1">
-            Git Finder
-          </Typography>
+          <Box>
+            <FormGroup>
+              <FormControlLabel
+                control={<Switch checked={theme.darkTheme} onChange={handleChange} />}
+                label="Toggle Theme"
+              />
+            </FormGroup>
+          </Box>
         </Toolbar>
       </AppBar>
-    </>
+    </React.Fragment>
   );
 };
 
