e1c361b22fdc43d388b69e57cd7a5c9d70e486ef
mina bessada <mina.adibe@gmail.com>
2022-08-05 10:30:50 +0200

    feat(project): add redux store and connrct it with app.js component

---
+++ src/store/store.tsx
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { githubApi } from "../api/github";
+import usersReducers from "./usersSlice";
+
+export const store = configureStore({
+  reducer: {
+    users: usersReducers,
+    [githubApi.reducerPath]: githubApi.reducer,
+  },
+  // adding the api middleware enables caching, invalidation, polling and other features of `rtk-query`
+
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(githubApi.middleware),
+  devTools: process.env.NODE_ENV !== "production",
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
