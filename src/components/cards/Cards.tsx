84f402d8204aa0c63fb4c56133c0ec9432932466
mina bessada <mina.adibe@gmail.com>
2022-08-06 13:40:17 +0200

    feat(cards component): add image when there is no search

--- src/components/cards/Cards.tsx
+++ src/components/cards/Cards.tsx
@@ -9,27 +9,38 @@ const Cards = (props: CardsProps) => {
   const { isFetching, isError, data } = props;
 
   if (isFetching) {
-    return (
-      <Box sx={{ display: "flex", alignItems: "center" }}>
-        <Loader />
-      </Box>
-    );
+    return <Loader />;
   }
   if (isError) {
     return <h1>Error...</h1>;
   }
   return (
     <React.Fragment>
-      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-        {data &&
-          data.items.map((user) => {
-            return (
-              <Grid item xs={2} sm={4} md={4} key={user.id}>
-                <UserCard user={user} />
-              </Grid>
-            );
-          })}
-      </Grid>
+      <Box
+        sx={{
+          width: "80vw",
+          mx: "auto",
+          display: "flex",
+          alignItems: "center",
+          my: "40px",
+          p: "15px",
+        }}>
+        {!data && (
+          <Box>
+            <img src="/assets/Home-img.svg" alt="React Logo" />
+          </Box>
+        )}
+        <Grid container spacing={{ xs: 2, md: 3 }}>
+          {data &&
+            data.items.map((user) => {
+              return (
+                <Grid item xs={12} sm={6} md={4} key={user.id}>
+                  <UserCard user={user} />
+                </Grid>
+              );
+            })}
+        </Grid>
+      </Box>
     </React.Fragment>
   );
 };
