85f949c10d11bb2057f9f3593d7500a4b937a9ff
mina bessada <mina.adibe@gmail.com>
2022-08-07 05:42:50 +0200

    refactor(project): delete unused elements

--- src/components/cardSkeleton/cardSkeleton.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const cardSkeleton = () => {
-  return <React.Fragment>cardSkeleton</React.Fragment>;
-};
-
-export default cardSkeleton;
-
-//new Array(5).fill('element')
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,7 +2,11 @@ import { Box } from "@mui/material";
 import Header from "../haeder/Header";
 
 const Layout = (props: any) => (
-  <Box sx={{ padding: "20px", marginTop: "63px" }}>
+  <Box
+    sx={{
+      padding: "20px",
+      marginTop: "63px",
+    }}>
     <Header />
 
     {props.children}
